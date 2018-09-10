df7122ca9437e3ac0c422f8ea4325dec680de08e
Nikita Krivtsov <nikikrschannel@gmail.com>
2018-09-10 21:30:20 +0300

    ЛР №2

---
+++ Geometry/Geometry/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Geometry {
+    class Program {
+
+        static void Main(string[] args) {
+            Circle c1 = new Circle(4);
+            Square s1 = new Square(10);
+            Rectangle r1 = new Rectangle(20, 4);
+            c1.Print();
+            s1.Print();
+            r1.Print();
+            Console.ReadLine();
+        }
+    }
+}
